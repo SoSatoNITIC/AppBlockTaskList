c5776cb1ab3c0c82002b16de2de63d04faec08dd
shun0773 <ynshun0730@gmail.com>
2023-11-17 10:00:29 +0900

    追加しました。最後のレイアウトです。

--- app/src/main/java/com/example/appblocktasklist/MainActivity.kt
+++ app/src/main/java/com/example/appblocktasklist/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.appblocktasklist
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.task_setting_should)
     }
 }
