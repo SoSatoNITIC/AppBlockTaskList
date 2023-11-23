cf03285d2fa570d870c1a5b414e291497894905c
IH <hfffdde2004@gmail.com>
2023-11-23 16:59:14 +0900

    受け取ったidによって処理を変更

--- app/src/main/java/com/example/appblocktasklist/TaskSettingWan.kt
+++ app/src/main/java/com/example/appblocktasklist/TaskSettingWan.kt
@@ -8,12 +8,14 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.navArgs
 import com.example.appblocktasklist.roomdb.TasksDB.Task
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 
 class TaskSettingWan : Fragment() {
+    private val args: TaskSettingWanArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,26 +30,63 @@ class TaskSettingWan : Fragment() {
         val navHostFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
 
-        view.findViewById<Button>(R.id.button7).setOnClickListener{
-            val title = view.findViewById<EditText>(R.id.title).text.toString()
-            val titleReason = view.findViewById<EditText>(R.id.title_reason).text.toString()
-            val reasonOfReason = view.findViewById<EditText>(R.id.reason_of_reason).text.toString()
-            val memo = view.findViewById<EditText>(R.id.memo).text.toString()
+        val titleEditText = view.findViewById<EditText>(R.id.title)
+        val titleReasonEditText = view.findViewById<EditText>(R.id.title_reason)
+        val reasonOfReasonEditText = view.findViewById<EditText>(R.id.reason_of_reason)
+        val memoEditText = view.findViewById<EditText>(R.id.memo)
 
-            if (title != ""){
-                GlobalScope.launch {
-                    MyApplication.database.tasksDao().insertAll(
-                        Task(
-                            taskName = title,
-                            memo = memo,
-                            reason = "$titleReason, $reasonOfReason"
+        if (args.taskID == -1) {
+            view.findViewById<Button>(R.id.button7).setOnClickListener{
+                val title = titleEditText.text.toString()
+                val titleReason = titleReasonEditText.text.toString()
+                val reasonOfReason = reasonOfReasonEditText.text.toString()
+                val memo = memoEditText.text.toString()
+
+                if (title != ""){
+                    GlobalScope.launch {
+                        MyApplication.database.tasksDao().insertAll(
+                            Task(
+                                taskName = title,
+                                memo = memo,
+                                reason = "$titleReason, $reasonOfReason"
+                            )
                         )
-                    )
+                    }
+                    val action = TaskSettingWanDirections.actionTaskSettingWanFragmentToSystemHomeFragment()
+                    navController.navigate(action)
                 }
-                val action = TaskSettingWanDirections.actionTaskSettingWanFragmentToSystemHomeFragment()
-                navController.navigate(action)
             }
+        } else {
+            GlobalScope.launch {
+                var task = MyApplication.database.tasksDao().getTask(args.taskID)
 
+                val reasons = task.reason.split(",")
+
+                titleEditText.setText(task.taskName)
+                titleReasonEditText.setText(reasons[0])
+                reasonOfReasonEditText.setText(reasons[1])
+                memoEditText.setText(task.memo)
+
+                view.findViewById<Button>(R.id.button7).setOnClickListener{
+                    val title = titleEditText.text.toString()
+                    val titleReason = titleReasonEditText.text.toString()
+                    val reasonOfReason = reasonOfReasonEditText.text.toString()
+                    val memo = memoEditText.text.toString()
+
+                    if (title != ""){
+                        GlobalScope.launch {
+                            task.taskName = title
+                            task.reason = "${titleReason}, ${reasonOfReason}"
+                            task.memo = memo
+                            MyApplication.database.tasksDao().update(
+                                task
+                            )
+                        }
+                        val action = TaskSettingWanDirections.actionTaskSettingWanFragmentToSystemHomeFragment()
+                        navController.navigate(action)
+                    }
+                }
+            }
         }
     }
 }
