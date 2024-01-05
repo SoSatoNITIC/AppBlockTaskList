64215cc5b2ba3dfb0115dc7169609f19f0b1d30c
SO <sosa035204@gmail.com>
2024-01-05 09:52:55 +0900

    対象アプリを見つけたら関数呼び出し

--- app/src/main/java/com/example/appblocktasklist/worker/UsedApp.kt
+++ app/src/main/java/com/example/appblocktasklist/worker/UsedApp.kt
@@ -10,6 +10,8 @@ import android.os.PowerManager
 class UsedApp(context: Context, params: WorkerParameters) : CoroutineWorker(context, params) {
     private var mostRecentlyUsedPackage: String? = null
 
+    var appName = mutableListOf<String>("com.google.android.youtube","com.google.android.apps.youtube.music",)
+
     override suspend fun doWork(): Result {
 
         //println("call doWork")
@@ -23,11 +25,10 @@ class UsedApp(context: Context, params: WorkerParameters) : CoroutineWorker(cont
             //画面がスリープされているときにカウントしないようにする
             val powerManager = applicationContext.getSystemService(Context.POWER_SERVICE) as PowerManager
 
-            delay(1000L)
-            //Thread.sleep(1000) // wait for 1 seconds
+            delay(1000L) // wait for 1 seconds
 
             // Query and aggregate usage stats
-            val statsMap = usageStatsManager.queryAndAggregateUsageStats(time - 1000 * 60 * 60 * 24, time)
+            val statsMap = usageStatsManager.queryAndAggregateUsageStats(time - 1000 * 10 , time)//60* 60 * 24
             mostRecentlyUsedPackage = if (powerManager.isInteractive) {
                 statsMap.values.maxByOrNull { it.lastTimeUsed }?.packageName
             } else {
@@ -39,9 +40,20 @@ class UsedApp(context: Context, params: WorkerParameters) : CoroutineWorker(cont
             if (localMostRecentlyUsedPackage != null) {
                 Log.i("Most Recently Used App", localMostRecentlyUsedPackage)
             }
+
+            //対象アプリを見つける
+            for (i in appName){
+                if(localMostRecentlyUsedPackage == i){
+                    hirakuFunction()
+                }
+            }
         }
 
         return Result.success()
+    }
+
+    fun hirakuFunction(){
+        return println("findApp!!")
     }
 
 }
