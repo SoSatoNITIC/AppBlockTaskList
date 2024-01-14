dc5269fff6b9ed39b76095b91a17e153f79aa07c
IH <hfffdde2004@gmail.com>
2024-01-14 20:29:05 +0900

    shield画面仮作成

---
+++ app/src/main/java/com/example/appblocktasklist/Shield.kt
@@ -0,0 +1,16 @@
+package com.example.appblocktasklist
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+class Shield: AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.shield)
+    }
+}
