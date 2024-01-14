60101795500402def422a242d67976e55517432f
IH <hfffdde2004@gmail.com>
2024-01-14 20:31:23 +0900

    db型変更

--- app/src/main/java/com/example/appblocktasklist/roomdb/rocksettingDB/RockSetting.kt
+++ app/src/main/java/com/example/appblocktasklist/roomdb/rocksettingDB/RockSetting.kt
@@ -1,16 +1,27 @@
 package com.example.appblocktasklist.roomdb.rocksettingDB
 
 import androidx.room.*
+import java.time.DayOfWeek
+import java.time.Duration
+import java.time.LocalTime
 
 @Entity
 data class RockSetting(
     @PrimaryKey(autoGenerate = true)
     val id: Int=0,
-    val timeRange: String,
-    val usableTime: Int,
-    val dayOfWeek: String,
+//    val timeRange: String,
+    val beginTime: LocalTime?,
+    val endTime: LocalTime?,
+    val usableTime: Duration?,
+    val dayOfWeek: List<DayOfWeek>,
     val targetApp: String,
-    val unusableTime: Int,
-    val preNoticeTiming: String,
-    val activeDate: String,
-)
+    val unUsableTime: Duration,
+    val preNoticeTiming: List<Duration>,
+    val activeDate: String?,
+) {init {
+    require(((beginTime == null && endTime == null) && usableTime != null) ||
+            ((beginTime != null && endTime != null) && usableTime == null)) {
+        "Exactly one of the columns must be non-null"
+    }
+}
+}
