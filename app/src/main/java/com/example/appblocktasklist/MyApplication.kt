bb435b306ec1333dd0171cf6832f91089adec8fd
IH <hfffdde2004@gmail.com>
2024-01-14 20:32:15 +0900

    usageStatsManagerを追加

--- app/src/main/java/com/example/appblocktasklist/MyApplication.kt
+++ app/src/main/java/com/example/appblocktasklist/MyApplication.kt
@@ -2,11 +2,14 @@ package com.example.appblocktasklist
 
 import android.app.Application
 import androidx.room.Room
+import com.example.appblocktasklist.lockProcess.GetterUsageStats
 import com.example.appblocktasklist.roomdb.AppDB
+import kotlin.reflect.KProperty
 
 class MyApplication : Application() {
     companion object {
         lateinit var database: AppDB
+        lateinit var usageGetter: GetterUsageStats
     }
 
     override fun onCreate() {
@@ -15,5 +18,6 @@ class MyApplication : Application() {
             applicationContext,
             AppDB::class.java, "AppDB"
         ).build()
+        usageGetter = GetterUsageStats(applicationContext)
     }
 }
