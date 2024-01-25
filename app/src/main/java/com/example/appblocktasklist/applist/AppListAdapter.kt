b7270e205a4f7701811db9f40618377b92c7c960
IH <hfffdde2004@gmail.com>
2024-01-26 07:48:08 +0900

    app list adapterにappInfoを直接保存するようにした

--- app/src/main/java/com/example/appblocktasklist/applist/AppListAdapter.kt
+++ app/src/main/java/com/example/appblocktasklist/applist/AppListAdapter.kt
@@ -2,7 +2,8 @@ package com.example.appblocktasklist.applist
 
 
 import android.content.Context
-import android.graphics.drawable.Drawable
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,17 +12,18 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.example.appblocktasklist.R
 
-class AppListAdapter(context: Context, private val appIcons: List<Drawable>, private val appNames: List<String>) :
-    ArrayAdapter<String>(context, R.layout.app_list_item, appNames) {
-
+class AppListAdapter(context: Context, private val appInfo: List<ApplicationInfo>) :
+    ArrayAdapter<ApplicationInfo>(context, R.layout.app_list_item, appInfo) {
+    private val pm: PackageManager = context.packageManager
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val info = appInfo[position]
         val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.app_list_item, parent, false)
 
         val iconView = view.findViewById<ImageView>(R.id.app_icon)
         val nameView = view.findViewById<TextView>(R.id.app_name)
 
-        iconView.setImageDrawable(appIcons[position])
-        nameView.text = appNames[position]
+        iconView.setImageDrawable(info.loadIcon(pm))
+        nameView.text = info.loadLabel(pm).toString()
 
         return view
     }
