49fc8c4d0ef94848475ba01bbf4457b3ab1258f0
IH <hfffdde2004@gmail.com>
2024-01-26 21:41:56 +0900

    fix LockSettingDetails

--- app/src/main/java/com/example/appblocktasklist/LockSettingDetails.kt
+++ app/src/main/java/com/example/appblocktasklist/LockSettingDetails.kt
@@ -1,32 +1,23 @@
 package com.example.appblocktasklist
 
-import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
-import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ListView
-import android.widget.ScrollView
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.NavHostFragment
 import com.example.appblocktasklist.applist.AppListAdapter
-import com.example.appblocktasklist.roomdb.rocksettingDB.LockSetting
-import com.example.appblocktasklist.roomdb.rocksettingDB.lockSettingDao
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.time.DayOfWeek
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
@@ -34,15 +25,8 @@ import java.util.Locale
 class LockSettingDetails : Fragment() {
     private val sharedViewModel: LockViewModel by activityViewModels()
 
-    // chosenAppIconsとchosenAppNamesをフラグメントのプロパティとして定義
-    private val chosenAppIcons = ArrayList<Drawable>()
-    private val chosenAppNames = ArrayList<String>()
-
-
-
-
-    fun convertEnglishDayToJapanese(day: String): String {
-        return when (day.toUpperCase()) {
+    private fun convertEnglishDayToJapanese(day: String): String {
+        return when (day.uppercase(Locale.ROOT)) {
             "MONDAY" -> "月"
             "TUESDAY" -> "火"
             "WEDNESDAY" -> "水"
@@ -54,13 +38,9 @@ class LockSettingDetails : Fragment() {
         }
     }
 
-    fun convertEnglishDaysToJapanese(days: String): String {
+    private fun convertEnglishDaysToJapanese(days: String): String {
         return days.split(",").map { convertEnglishDayToJapanese(it.trim()) }.joinToString(", ")
     }
-
-
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -77,13 +57,7 @@ class LockSettingDetails : Fragment() {
         val notificationTimings: ArrayList<String> = ArrayList()
         val notificationTimingList: ListView = view.findViewById(R.id.notifyList)
 
-
-
-
-
-
-
-        sharedViewModel.preNoticeTiming.observe(viewLifecycleOwner, Observer { preNoticeTimings ->
+        sharedViewModel.preNoticeTiming.observe(viewLifecycleOwner) { preNoticeTimings ->
             // preNoticeTimingsが更新されたときの処理を書く
             // 例えば、preNoticeTimingsをログに出力する
             println(preNoticeTimings)
@@ -103,13 +77,14 @@ class LockSettingDetails : Fragment() {
             // 画面遷移後にListViewを更新
             coroutineScope.launch {
                 delay(100L) // 0.1秒待機
-                val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, notificationTimings)
+                val adapter = ArrayAdapter(
+                    requireContext(),
+                    android.R.layout.simple_list_item_1,
+                    notificationTimings
+                )
                 notificationTimingList.adapter = adapter
             }
-        })
-
-
-
+        }
 
 
         //NavHostの取得
@@ -121,118 +96,56 @@ class LockSettingDetails : Fragment() {
 
         val packageManager: PackageManager = requireActivity().packageManager
 
-        val installedApplications = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
-
-        // システムアプリを除外する Youtube関係は保持
-        val userApps = installedApplications.filter { appInfo ->
-            (appInfo.flags and ApplicationInfo.FLAG_SYSTEM) == 0 ||
-                    appInfo.packageName == "com.google.android.youtube" ||
-                    appInfo.packageName == "com.google.android.apps.youtube.music" ||
-                    appInfo.packageName == "com.android.chrome"
-        }
-        //AppNameとIcon表示
-        val appNames = userApps.map { it.loadLabel(packageManager).toString() }
-
-        val appIcons = userApps.map { it.loadIcon(packageManager) }
-
-        val appKey = appNames.zip(appIcons).toMap()
-
-        sharedViewModel.targetApp.observe(viewLifecycleOwner, Observer { apps ->
+        sharedViewModel.targetApp.observe(viewLifecycleOwner) { apps ->
             // appsは更新された値
             // ここでappsを使ってUIを更新する
 
-            // 選択されたアプリの名前とアイコンを取得
-            val appNames = apps.map { appName -> appName }
-            val appIcons = apps.map { appName -> appKey[appName] ?: ColorDrawable() }
-
-
-            // chosenAppNamesとchosenAppIconsを更新
-            chosenAppNames.clear()
-            chosenAppNames.addAll(appNames)
-            chosenAppIcons.clear()
-            chosenAppIcons.addAll(appIcons)
+            val chosenApps = apps.map { packageManager.getApplicationInfo(it, PackageManager.GET_META_DATA) }
 
             // ListViewのアダプターを更新
-            val adapterChoose = AppListAdapter(requireActivity(), chosenAppIcons, chosenAppNames)
+            val adapterChoose = AppListAdapter(requireActivity(), chosenApps)
             appListViewChoose.adapter = adapterChoose
 
-
             println(appListViewChoose)
-        })
+        }
 
-
-
-
-
-        sharedViewModel.usableTime.observe(viewLifecycleOwner, { usableTime ->
+        sharedViewModel.usableTime.observe(viewLifecycleOwner) { usableTime ->
+            val textView = view.findViewById<TextView>(R.id.locksettingtexts)
             if(usableTime == null){
                 println("Usable null")
-                val lockset = "時間帯型ロック"
-                var timeString = " "
-                sharedViewModel.beginTime.observe(viewLifecycleOwner, { beginTime ->
+                sharedViewModel.beginTime.observe(viewLifecycleOwner) { beginTime ->
                     val begintime = beginTime?.format(DateTimeFormatter.ofPattern("HH:mm"))
-                    sharedViewModel.endTime.observe(viewLifecycleOwner, { endTime ->
+                    sharedViewModel.endTime.observe(viewLifecycleOwner) { endTime ->
                         val endtime = endTime?.format(DateTimeFormatter.ofPattern("HH:mm"))
-                        timeString += "時間帯型ロック\n開始時間：$begintime\n終了時間：$endtime\n"
-
-                        sharedViewModel.dayOfWeek.observe(viewLifecycleOwner, { dayOfWeek ->
+                        textView.append("時間帯型ロック\n開始時間：$begintime\n終了時間：$endtime\n")
+                        sharedViewModel.dayOfWeek.observe(viewLifecycleOwner) { dayOfWeek ->
                             val selectedDays = dayOfWeek.entries.filter { it.value }.joinToString(", ") { it.key.name }
-                            //println(selectedDays)
                             val japaneseDays = convertEnglishDaysToJapanese(selectedDays)
-                            timeString += "制限する曜日：$japaneseDays\n"
-
-
-
-                            val textView = view.findViewById<TextView>(R.id.locksettingtexts)
-                            textView.text = timeString
-
-                        })
-
-                        println(timeString)
-                        //val textView = view.findViewById<TextView>(R.id.lockSetting)
-                        //textView.text = timeString
-
-                    })
-                })
-
-                
-            }else{
+                            textView.append("制限する曜日：$japaneseDays\n")
+                        }
+                    }
+                }
+            } else {
                 println("Begin and End Time null")
-                var timeString = " "
-                sharedViewModel.usableTime.observe(viewLifecycleOwner, { usableTime ->
+                sharedViewModel.usableTime.observe(viewLifecycleOwner) { usableTime ->
                     val hours = usableTime?.toHours() ?: 0
                     val minutes = usableTime?.toMinutesPart() ?: 0
                     val usableTimes = String.format("%02d時間%02d分", hours, minutes)
-                    timeString += "使用時間型ロック\n使用時間：$usableTimes　経過したら制限\n"
-                    sharedViewModel.dayOfWeek.observe(viewLifecycleOwner, { dayOfWeek ->
+                    textView.append("使用時間型ロック\n使用時間：$usableTimes　経過したら制限\n")
+                    sharedViewModel.dayOfWeek.observe(viewLifecycleOwner) { dayOfWeek ->
                         val selectedDays = dayOfWeek.entries.filter { it.value }.joinToString(", ") { it.key.name }
-                        println(selectedDays)
                         val japaneseDays = convertEnglishDaysToJapanese(selectedDays)
-                        timeString += "選択された曜日：$japaneseDays\n"
-
-
-                        val textView = view.findViewById<TextView>(R.id.locksettingtexts)
-                        textView.text = timeString
-
-                    })
-                })
+                        textView.append("選択された曜日：$japaneseDays\n")
+                    }
+                }
             }
-        })
+        }
 
-
-
-
-
-        sharedViewModel.lockid.observe(viewLifecycleOwner, Observer<Int?> { lockid ->
-            println(lockid)
-        })
-
-
-
+        sharedViewModel.lockid.observe(viewLifecycleOwner) { lockId ->
+            println(lockId)
+        }
 
         view.findViewById<Button>(R.id.button5).setOnClickListener{
-
-
             //sharedViewModel.saveSettings()
             CoroutineScope(Dispatchers.IO).launch {
                 sharedViewModel.saveSettings()
@@ -255,6 +168,4 @@ class LockSettingDetails : Fragment() {
             navController.navigate(action)
         }
     }
-
-
 }
