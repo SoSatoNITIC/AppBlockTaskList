bc97707c5926aa4a5bc0bef4d05d88b99a9def7d
SO <sosa035204@gmail.com>
2024-01-29 16:32:58 +0900

    test

--- app/src/main/java/com/example/appblocktasklist/Shield.kt
+++ app/src/main/java/com/example/appblocktasklist/Shield.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
@@ -12,5 +13,12 @@ class Shield: AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.shield)
+
+
+        val taskkustButton = findViewById<Button>(R.id.tasklist_GO_button)
+
+        taskkustButton.setOnClickListener{
+            println("buttonbnm,hnjkm,")
+        }
     }
 }
