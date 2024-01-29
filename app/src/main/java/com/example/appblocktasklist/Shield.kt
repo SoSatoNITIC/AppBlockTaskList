4a620e4137a1cdf5fad421b9cd34c0fca9746b69
SO <sosa035204@gmail.com>
2024-01-29 20:55:43 +0900

    シールド画面の修正とWorkerの常時起動処理

--- app/src/main/java/com/example/appblocktasklist/Shield.kt
+++ app/src/main/java/com/example/appblocktasklist/Shield.kt
@@ -1,6 +1,7 @@
 package com.example.appblocktasklist
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,8 +18,27 @@ class Shield: AppCompatActivity()  {
 
         val taskkustButton = findViewById<Button>(R.id.tasklist_GO_button)
 
+        val okbutton = findViewById<Button>(R.id.button2)
+
         taskkustButton.setOnClickListener{
-            println("buttonbnm,hnjkm,")
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("key", "locked") // ここでロック画面からの遷移
+            startActivity(intent)
         }
+
+        okbutton.setOnClickListener{
+
+            val intent2 = Intent(this, MainActivity::class.java)
+            intent2.putExtra("key", "locked") // ここでロック画面からの遷移
+            startActivity(intent2)
+
+
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.addCategory(Intent.CATEGORY_HOME)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
+
+
     }
 }
